12316b585e8c4c93a31617640435f4754052d98a
beastlywallace <akonakpevwe@gmail.com>
2022-10-24 23:35:12 +0100

    location

--- client/index.js
+++ client/index.js
@@ -137,3 +137,29 @@ function loadHTMLTable(data) {
 
     table.innerHTML = tableHtml;
 }
+
+
+const findMyState = () => {
+    const status = document.querySelector('.status')
+    
+    const success = (position) => {
+        console.log(position)
+       const latitude= position.coords.latitude
+        const longitude = position.coords.longitude
+
+        const geoApiUrl = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`
+        
+        fetch(geoApiUrl).then(res => res.json())
+            .then((data) => {
+                console.log(data)
+            status.textContent= data.principalSubdivision
+        })
+    }
+    const error = () => {
+
+        status.textContent='unable to retrieve location'
+    }
+    navigator.geolocation.getCurrentPosition(success, error)
+}
+
+document.getElementById('state').addEventListener('click', findMyState)
